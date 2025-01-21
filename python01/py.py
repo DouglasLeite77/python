04a3784c1c93bd1886bd32709a35c964703734bf
DouglasLeite77 <douglasleitee255@gmail.com>
2025-01-21 20:06:58 -0300

    .

--- python01/py.py
+++ python01/py.py
@@ -4,4 +4,7 @@ import numpy as np
 from abc import ABCMeta, abstractmethod
 @abstractmethod
 biblioteca para força um a implementação de um metodo m um objeto filho
+
+from functools import  total_ordering
+@total_ordering
 """
