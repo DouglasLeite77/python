100cedeaa9ccdc92f10495405361173b490e1972
DouglasLeite77 <douglasleitee255@gmail.com>
2025-08-11 16:57:30 -0300

    .

---
+++ curso_Teo/testes.py
@@ -0,0 +1,27 @@
+# %%
+import requests
+import json
+
+
+ceps = [
+    "45020300",
+    "45020700"
+]
+
+dados = []
+
+for i in ceps:
+
+    url = f'https://viacep.com.br/ws/{i}/json/'
+    resposta = requests.get(url)
+
+    if resposta.status_code == 200:
+        dados.append(resposta.json())
+
+dados
+
+# %%
+
+with open('ceps.json', 'w', encoding='utf-8') as file:
+    json.dump(dados, file, ensure_ascii=False, indent=4)
+# %%
