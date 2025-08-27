145f2c8b4086c2ace96273bcce0de0dd4513e57e
DouglasLeite77 <douglasleitee255@gmail.com>
2025-08-26 23:11:07 -0300

    Update 03.py

--- curso_Teo/pandas/03.py
+++ curso_Teo/pandas/03.py
@@ -1,3 +1,29 @@
-# %% 
+# %%
 import pandas as pd
+import requests
 
+url = "https://pt.wikipedia.org/wiki/Unidades_federativas_do_Brasil"
+
+headers = {"User-Agent": "Mozilla/5.0"}  # finge ser navegador
+response = requests.get(url, headers=headers)
+
+tables = pd.read_html(response.text)
+
+# %%
+ufs = tables[1]
+# %%
+
+ufs.head()
+# %%
+
+def verificação(linha):
+    return linha['Abreviação'] == 'AC'
+
+
+# %%
+ufs.apply(verificação, axis=1)
+# %%
+
+ufs.apply(lambda x: x['Abreviação'] == 'AC', axis=1)
+
+# %%
