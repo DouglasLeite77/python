015aaef9a06ec929f6c4cb76b417390a6424b5c1
DouglasLeite77 <douglasleitee255@gmail.com>
2025-09-09 20:46:34 -0300

    Update 08.py

--- curso_Teo/pandas/08.py
+++ curso_Teo/pandas/08.py
@@ -1,9 +1,19 @@
 # %%
 import pandas as pd
-import sqlalchemy 
+import sqlalchemy as sqal
 
 with open("etl.sql") as open_file:
     query = open_file.read()
 
 print(query)
+# %%
+
+
+engine = sqal.create_engine("sqlite:///./database.db")
+
+# %%
+
+cliente = pd.read_sql_table(table_name="clientes", con=engine)
+# %%
+cliente
 # %%
