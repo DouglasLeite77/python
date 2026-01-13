fa6254e4b57f660f34178b5f5500c19df99cb1f5
DouglasLeite77 <douglasleitee255@gmail.com>
2026-01-13 19:11:21 -0300

    Update medidas_de_resumo.py

--- estatística com python/medidas_de_resumo.py
+++ estatística com python/medidas_de_resumo.py
@@ -2,3 +2,36 @@
 import pandas as pd
 from pandasql import sqldf
 # %%
+
+df = pd.read_csv("./data/points_tmw - dados origem.csv")
+df.head()
+# %%
+media = df['qtdPontos'].mean()
+media
+# %%
+mediana =   df['qtdPontos'].median()
+mediana
+# %%
+minimo = df['qtdPontos'].min()
+maximo = df['qtdPontos'].max()
+minimo
+maximo
+# %%
+quartil_1 = df['qtdPontos'].quantile(0.25)
+print(quartil_1)
+quartil_2 = df['qtdPontos'].quantile(0.50)
+print(quartil_2)
+quartil_3 = df['qtdPontos'].quantile(0.75)
+print(quartil_3)
+quartil_4 = df['qtdPontos'].quantile(1)
+print(quartil_4)
+# %%
+df['qtdPontos'].describe()
+# %%
+
+usuario = df.groupby('idUsuario').agg({'qtdPontos':'sum', 'idTransacao':'count'}).reset_index()
+usuario
+
+# %%
+
+usuario[['idTransacao','qtdPontos']].describe()
