9857888666e69019d081c888e521bc990c6d080e
Douglas <douglasleitee255@gmail.com>
2026-04-17 11:26:27 -0300

    consulta apicep

--- pratica ETL/script.py
+++ pratica ETL/script.py
@@ -4,10 +4,36 @@ import requests
 
 # %%
 
-df_clientes = pd.read_csv('./data/clientes.csv', sep=',')
+df_clientes = pd.read_csv('./data/clientes.csv', sep=',', dtype={'cep': str})
 df_compras = pd.read_csv('./data/compras.csv', sep=',')
 
 df_clientes.head()
 # %%
 df_compras.head()
+# %%
+
+def consulta_cep(cep):
+    url = f'https://viacep.com.br/ws/{cep}/json/'
+    
+    dados = requests.get(url)
+    return dados.json()
+# %%
+
+def get_dados():
+    lista_ceps = df_clientes['cep'].astype(str).replace(',', '', regex=True)
+    
+    dados = []
+    
+    for i in lista_ceps:
+        dados.append(consulta_cep(i))
+    
+    df_dados = pd.DataFrame(dados)
+    df_clientes['logradouro'] = df_dados['logradouro']
+    df_clientes['bairro'] = df_dados['bairro']
+    df_clientes['localidade'] = df_dados['localidade']
+    df_clientes['uf'] = df_dados['uf']
+# %%
+
+get_dados()
+df_clientes.head()
 # %%
